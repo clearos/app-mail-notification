9dde739dbe3005b64bdf2e84d581a3dba48c09b8
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-03-28 11:39:05 +0000

    - Prep 1.0.10 snapshot

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'mail_notification';
-$app['version'] = '1.0.4';
+$app['version'] = '1.0.10';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
